23b3f7671671481f3f624b8d05871ee05639bc05
Lucas Menke <lucas_menke@icloud.com>
2023-01-04 18:21:42 +0100

    added product types admin page & services

--- Ecommerce/Server/RegisterServices.cs
+++ Ecommerce/Server/RegisterServices.cs
@@ -24,6 +24,7 @@ public static class RegisterServices
         builder.Services.AddScoped<IOrderService, OrderService>();
         builder.Services.AddScoped<IPaymentService, PaymentService>();
         builder.Services.AddScoped<IAddressService, AddressService>();
+        builder.Services.AddScoped<IProductTypeService, ProductTypeService>();
         builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             .AddJwtBearer(options =>
             {
